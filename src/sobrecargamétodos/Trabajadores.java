e36f9163f3c2fc559bd5f6c16971abe313227778
TreizyHoyos <treizyyadhirahoyosjulca@gmail.com>
2026-04-26 19:43:45 -0500

    Creación de las variables

---
+++ src/sobrecargamétodos/Trabajadores.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sobrecargamétodos;
+
+/**
+ *
+ * @author treiz
+ */
+public class Trabajadores 
+{
+    private String tipoDoc;
+    private String NDoc; 
+    private String paterno; 
+    private String materno; 
+    private String nombre;
+    private String celular;
+    private String correo;
+    private String regimenLab;
+    private String regimenPen;
+    private String codigoInterno;
+    
+}
