fa11e9f95d02a9e96a335e5e0f5686fe8553062d
castillo13alison-lgtm <castillo.13.alison@gmail.com>
2026-04-26 21:58:41 -0500

    Salida de datos de los trabajadores

--- src/sobrecargamétodos/SobrecargaMétodos.java
+++ src/sobrecargamétodos/SobrecargaMétodos.java
@@ -45,7 +45,42 @@ public class SobrecargaMétodos {
         trabajador3.setCorreo("ana.lopez@gmail.com");
         trabajador3.setRegimenPensionario("ONP");
         
+        System.out.println("Trabajador 1:");
+        System.out.println("Codigo Interno: " + trabajador1.getCodigoInterno());
+        System.out.println("Tipo Documento: " + trabajador1.getTipoDocumento());
+        System.out.println("Numero Documento: " + trabajador1.getNumeroDocumento());
+        System.out.println("Apellidos y Nombres: " + trabajador1.getApellidoPaterno() + " " +
+                           trabajador1.getApellidoMaterno() + ", " + trabajador1.getNombres());
+        System.out.println("Celular: " + trabajador1.getCelular());
+        System.out.println("Correo: " + trabajador1.getCorreo());
+        System.out.println("Regimen Laboral: " + trabajador1.getRegimenLaboral());
+        System.out.println("Regimen Pensionario: " + trabajador1.getRegimenPensionario());
+        System.out.println();
+
+       
+        System.out.println("Trabajador 2:");
+        System.out.println("Codigo Interno: " + trabajador2.getCodigoInterno());
+        System.out.println("Tipo Documento: " + trabajador2.getTipoDocumento());
+        System.out.println("Numero Documento: " + trabajador2.getNumeroDocumento());
+        System.out.println("Apellidos y Nombres: " + trabajador2.getApellidoPaterno() + " " +
+                           trabajador2.getApellidoMaterno() + ", " + trabajador2.getNombres());
+        System.out.println("Celular: " + trabajador2.getCelular());
+        System.out.println("Correo: " + trabajador2.getCorreo());
+        System.out.println("Regimen Laboral: " + trabajador2.getRegimenLaboral());
+        System.out.println("Regimen Pensionario: " + trabajador2.getRegimenPensionario());
         
+        System.out.println();
+
+        System.out.println("Trabajador 3:");
+        System.out.println("Codigo Interno: " + trabajador3.getCodigoInterno());
+        System.out.println("Tipo Documento: " + trabajador3.getTipoDocumento());
+        System.out.println("Numero Documento: " + trabajador3.getNumeroDocumento());
+        System.out.println("Apellidos y Nombres: " + trabajador3.getApellidoPaterno() + " " +
+                           trabajador3.getApellidoMaterno() + ", " + trabajador3.getNombres());
+        System.out.println("Celular: " + trabajador3.getCelular());
+        System.out.println("Correo: " + trabajador3.getCorreo());
+        System.out.println("Regimen Laboral: " + trabajador3.getRegimenLaboral());
+        System.out.println("Regimen Pensionario: " + trabajador3.getRegimenPensionario());
         
     }
     
