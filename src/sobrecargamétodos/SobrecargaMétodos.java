6851cb248e4939df70432fd5d18d8153c584d6c2
denisromero025 <denisromero025@gmail.com>
2026-04-26 21:49:17 -0500

    Creación de un trabajador usando el constructor vacío

--- src/sobrecargamétodos/SobrecargaMétodos.java
+++ src/sobrecargamétodos/SobrecargaMétodos.java
@@ -15,6 +15,20 @@ public class SobrecargaMétodos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        // Crear un trabajador usando el constructor vacío
+        Trabajadores trabajador1 = new Trabajadores();
+        trabajador1.setTipoDocumento("DNI");
+        trabajador1.setNumeroDocumento("60756421");
+        trabajador1.setApellidoPaterno("Perez");
+        trabajador1.setApellidoMaterno("Gomez");
+        trabajador1.setNombres("Juan");
+        trabajador1.setCelular("942369542");
+        trabajador1.setCorreo("juan.perez@gmail.com");
+        trabajador1.setRegimenLaboral("CAS");
+        trabajador1.setRegimenPensionario("AFP");
+        
+        
     }
     
 }
